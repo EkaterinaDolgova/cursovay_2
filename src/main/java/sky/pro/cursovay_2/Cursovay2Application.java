0b6d865637367d01ccacc83499355ea03f04183c
katya <kotenokches@rambler.ru>
2022-05-10 10:46:48 +0500

    Первый коммит

---
+++ src/main/java/sky/pro/cursovay_2/Cursovay2Application.java
@@ -0,0 +1,13 @@
+package sky.pro.cursovay_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursovay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursovay2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/cursovay_2/Cursovay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.cursovay_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursovay2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
